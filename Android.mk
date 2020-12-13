038b98bf2823f2f550acce3d3cd6c4c2fc6717db
eva0034 <30981083+eva0034@users.noreply.github.com>
2020-12-13 19:13:40 +1100

    Add support for Samsung S10 5G (beyondx)
    Adding support for Samsung S10 5G (beyondx)

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter beyond0lte beyond1lte beyond2lte, $(TARGET_DEVICE)),)
+ifneq ($(filter beyondx beyond0lte beyond1lte beyond2lte, $(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
